b44600f1c985d28f19f625de7f1f5b44307c9d82
huhaiguo <huhaiguo@zkdntech.com>
2021-01-04 16:10:40 +0800

    feart 前端包

---
+++ packages/font/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="love-code">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
