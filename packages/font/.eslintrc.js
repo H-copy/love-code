98694fa785a597b90a54e4b566e791b41f14188c
copy-left <1123605771@qq.com>
2021-01-05 00:06:14 +0800

    test 测试complie新方法

--- packages/font/.eslintrc.js
+++ packages/font/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'comma-dangle': ['off'],
+    'linebreak-style': 'off',
     'semi': ["warn", "never", { "beforeStatementContinuationChars": "always"}] 
   },
   overrides: [
--- packages/font/src/views/Home.vue
+++ packages/font/src/views/Home.vue
@@ -5,6 +5,26 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import {
+  createTagNode,
+  createTextNode,
+  createDynamicProp,
+  html
+} from '@love-code/complie'
+
+const root = createTagNode(
+  'div',
+  createDynamicProp(
+    'style',
+    {
+      color: 'orange',
+      fontSize: '14px'
+    }
+  ),
+  createTextNode('show me')
+)
+
+console.log('>>>', root, html.parse(root))
 
 export default defineComponent({
   setup() {
