0034a66bed46817ad4c3b3bb9caa81c4aea8117c
huhaiguo <huhaiguo@zkdntech.com>
2021-01-06 18:27:58 +0800

    refactor

--- packages/complie/__test__/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import {
-  oneSpace
-} from '../src/utils'
-
-
-test('oneSpace', () => {
-  expect(oneSpace('a    b   c')).toBe('a b c')
-})
--- packages/complie/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * as node from './node'
-export * as html from './html'
-export * from './node-pro'
-
-
