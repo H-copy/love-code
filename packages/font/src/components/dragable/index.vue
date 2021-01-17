398a13a71a53bc579e8d8c6c73ffe86d17708191
copy-left <1123605771@qq.com>
2021-01-17 13:40:20 +0800

    feart 拖拽组件

--- packages/font/src/components/dragable/index.vue
+++ packages/font/src/components/dragable/index.vue
@@ -12,6 +12,7 @@ $color = orange
     border 1px solid $color
     background white
     transition all .3s
+    z-index: 1
     &:hover{
       transform scale(1.5, 1.5)
     }
@@ -69,199 +70,49 @@ $color = orange
 }
 </style>
 <template>
-  <div class='dragable' :style='containerStyle'>
+  <div class='dragable' :style='moveBlockStyle' @mousedown.stop='onMouseDown' >
     <slot></slot>
       <div
       class='dragable-point'
-      v-for='type of points'
-      @mousedown.stop='e => onPointMousedown(e, type)'
+      v-for='type of movePoints'
+      @mousedown.stop='e => onPointMousedown(e, moveBlock, type)'
       :class='type'
       :key='type'>
       </div>
   </div>
 </template>
 <script lang='ts'>
-import { computed, defineComponent, ref } from 'vue'
-
-type Point = 'topLeft' 
-              | 'topRight' 
-              | 'middleLeft' 
-              | 'middleRight' 
-              | 'bottomLeft' 
-              | 'bottomRight'
-              | 'middleTop'
-              | 'middleBottom'
-
-const points: Point[] = [
-  'topLeft',
-  'topRight',
-  'bottomLeft',
-  'bottomRight',
-  'middleTop',
-  'middleBottom',
-  'middleLeft',
-  'middleRight',
-]
-
-interface Position {
-  x: number
-  y: number
-}
-
-interface PointEvent {
-  [k: string]: (diff: Position) => void
-}
+import { defineComponent } from 'vue'
+import {
+  useMoveBlock,
+  useMovePoint,
+  movePoints,
+  MoveBlock
+} from './hooks'
 
 export default defineComponent({
-  setup() {
-    const unit = 'px'
-    const containerSize = ref<{[k: string]: number}>({
-      width: 120,
-      height: 120,
-      top: 10,
-      bottom: 0,
-      left: 10,
-      right: 0,
-    })
-    const updateContainerSize = (data: {[k: string]: number }) => {
-      const _data = { ...containerSize.value, ...data }
-      if (_data.width <= 0){
-        _data.width = 0
-      }
-      if (_data.height <= 0){
-        _data.height = 0
-      }
-      containerSize.value = _data
+  setup(){
+    const {
+      moveBlock,
+      moveBlockStyle,
+      updateBlock,
+      mouseMoveLock,
+      onMouseDown
+    } = useMoveBlock()
+    
+    const update = (d: MoveBlock) => {
+      updateBlock(mouseMoveLock(d))
     }
-    const startState = ref({
-      preContainer: { ...containerSize.value },
-      type: '',
-      startX: 0,
-      startY: 0
-    })
-    const containerStyle = computed(() => {
-      return Object.entries(containerSize.value).reduce((acc, [key, val]) => {
-        return { ...acc, [key]: `${val}${unit}` }
-      }, {})
-    })
-    const round = (n: number) => Math.round(n)
-    const mouseMoveLock = (d: {[k: string]: number}) => {
-      // 防止尺寸为零时，元素移动
-      const _d = { ...d }
-      if (_d.width <= 0){
-        _d.width = 0
-        _d.left = containerSize.value.left
-      }
-      
-      if (_d.height <= 0){
-        _d.height = 0
-        _d.top = containerSize.value.top
-      }
-      return _d
-    }
-    const pointEvents: PointEvent = {
-      topLeft: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          width: preContainer.width - diff.x,
-          height: preContainer.height - diff.y,
-          top: preContainer.top + diff.y,
-          left: preContainer.left + diff.x,
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-      topRight: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          width: preContainer.width + diff.x,
-          height: preContainer.height - diff.y,
-          top: preContainer.top + diff.y,
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-      middleTop: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          height: preContainer.height - diff.y,
-          top: preContainer.top + diff.y,
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-      middleBottom: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          height: preContainer.height + diff.y,
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-      middleLeft: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          width: preContainer.width - diff.x,
-          left: preContainer.left + diff.x,
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-      middleRight: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          width: preContainer.width + diff.x,
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-      bottomLeft: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          width: preContainer.width - diff.x,
-          height: preContainer.height + diff.y,
-          left: preContainer.left + diff.x,
-        }
-        if (_d.width <= 0){
-          _d.width = 0
-          _d.left = containerSize.value.left
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-      bottomRight: (diff: Position) => {
-        const { preContainer } = startState.value
-        const _d = {
-          width: preContainer.width + diff.x,
-          height: preContainer.height + diff.y
-        }
-        updateContainerSize(mouseMoveLock(_d))
-      },
-    }
-
-    const onPointMousedown = (e: MouseEvent, t: Point) => {
-      startState.value = {
-        type: t,
-        preContainer: { ...containerSize.value },
-        startX: e.clientX,
-        startY: e.clientY
-      }
-      
-      const size = { ...containerSize.value }
-      const cb = (event: MouseEvent) => {
-        const { startX, startY, type } = startState.value
-        const diff = {
-          x: event.clientX - startX,
-          y: event.clientY - startY 
-        }
-        const setFn = pointEvents[type]
-        if (setFn){
-          setFn(diff)
-        }
-      }
-      
-      document.addEventListener('mousemove', cb)
-      document.addEventListener('mouseup', () => {
-        document.removeEventListener('mousemove', cb)
-      })
-    }
-    return {
-      points,
-      containerStyle,
+    const {
+      startState,
       onPointMousedown
+    } = useMovePoint(update)
+    return {
+      movePoints,
+      moveBlockStyle,
+      moveBlock,
+      onPointMousedown,
+      onMouseDown
     }
   }
 })
