c1d69ecb8ad0ae8acc185ced7a3a1fc4bd2c68df
huhaiguo <huhaiguo@zkdntech.com>
2021-01-20 14:41:57 +0800

    feart 渲染器

--- packages/font/src/components/render/render.tsx
+++ packages/font/src/components/render/render.tsx
@@ -1,6 +1,7 @@
 import {
   computed,
   defineComponent,
+  isRef,
   PropType,
   ref,
   Ref
@@ -8,17 +9,23 @@ import {
 
 import {
   Tag,
-  isNativeTag,
   isDynamicTag,
   isSlefTag,
   isTextTag,
 
   isNativeProp,
   isSlefProp,
-  isEventProp
+  isEventProp,
+  isDynamicProp,
+  isIfProp,
+  isModelProp,
+  // isRefProp,
+  // isSlotProp,
+  // isForProp
 } from '@love-code/compile'
 
 import * as assert from '../../utils/assert'
+import { firstUpperCase } from '../../utils'
 
 function render(node: Tag, globalCtx: Ref<any>){
   const Cmp = computed(() => {
@@ -30,16 +37,63 @@ function render(node: Tag, globalCtx: Ref<any>){
       return {}
     }
     return node.props.reduce((acc: any, next: any) => {
+      // baseType
       if (isNativeProp(next) || isSlefProp(next)){
         return { ...acc, [next.name]: next.value }
       }
-
+      
+      // vue directive
+      
+      // event
       if (isEventProp(next)){
         const { directive, value } = next
-        const eventName = directive.arg
+        let eventName = directive.arg
         const _p = assert.isString(next.value) ? globalCtx.value[next.value] : value
-        return { ...acc, [eventName]: _p }
+        eventName = assert.isEventName(eventName) ? eventName : `on${firstUpperCase(eventName)}`
+        return assert.isFunction(_p) ? { ...acc, [eventName]: _p } : acc
       }
+      
+      if (isDynamicProp(next)){
+        const { directive, value } = next
+        const propName = directive.arg
+        const _p = assert.isString(propName) ? globalCtx.value[propName] : value
+        return { ...acc, [propName]: _p }
+      }      
+
+      if (isIfProp(next)){
+        let _p = assert.isString(next.value) ? globalCtx.value[next.value] : next.value
+        _p = assert.isFunction(_p) ? _p() : _p
+        return { ...acc, IF: assert.isBoolean(_p) ? _p : !!_p }
+      }
+
+      if (isModelProp(next)){
+        const { directive, value } = next
+        const { arg, isDynamic } = directive
+        let _arg = arg
+        if (isDynamic){
+          _arg = globalCtx.value[arg]
+        }
+        
+        if (assert.isString(value)){
+          /* eslint-disable-next-line no-param-reassign */
+          return { ...acc, [_arg]: globalCtx.value[value], [`onUpdate:${_arg}`]: (data: any) => { globalCtx.value[value] = data } }
+        }
+
+        /* eslint-disable-next-line no-param-reassign */
+        return {
+          ...acc,
+          [_arg]: isRef(value) ? value.value : value,
+          [`onUpdate:${_arg}`]: (data: any) => {
+            const _v = isRef(value) ? value : next
+            _v.value = data    
+          }
+        }
+      }
+
+      // TODO
+      // if (isRefProp(next)){}
+      // if (isSlotProp(next)){}
+      // if (isForProp(next)){}
       
       return { ...acc, next }
     }, {})
@@ -52,12 +106,21 @@ function render(node: Tag, globalCtx: Ref<any>){
   if (isSlefTag(node)){
     return <Cmp.value {..._props} />
   }
-  
+
+  if (isDynamicTag(node)){
+    return globalCtx.value[node.tag]
+  }
+
   const _children = computed<Tag[]>(() => {
     const _c: Tag[] = (node as Tag).children || []
     return _c.map((child: Tag) => render(child, globalCtx))
   })
-  return <Cmp.value {..._props.value}> { _children.value } </Cmp.value>
+
+  if (_props.value.IF === false){
+    return null
+  }
+  
+  return <Cmp.value { ..._props.value }> { _children.value } </Cmp.value>
 }
 
 const Tag = defineComponent({
@@ -73,10 +136,15 @@ const Tag = defineComponent({
     },
   },
   setup(props) {
-    const globalCtx = ref({
-      submit: () => console.log('submit')
-    })
-    return () => render(props.node, globalCtx)
+    const globalCtx = ref({ ...props.globalCtx })
+    return () => {
+      try {
+        return render(props.node, globalCtx)
+      } catch (error) {
+        console.log('RENDER ERR:', error)
+        return error
+      }
+    }
   }
 })
 
